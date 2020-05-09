487cc62773317d5551ef28674b12d7c0636ac1c0
Haldir65 <mjw090608@gmail.com>
2020-05-09 10:24:42 +0800

    updates

--- python/fuckgradle.py
+++ python/fuckgradle.py
@@ -20,59 +20,63 @@ APP_BUILD_GRADLE_FILE = "app/build.gradle"
 ## ext.kotlin_version = '1.3.61'
 
 EXT_KOTLIN_PATTERN="ext.kotlin_version"
-EXT_KOTLIN_PATTERN_REPLACEMENT="ext.kotlin_version = '1.3.61'\n"
+EXT_KOTLIN_PATTERN_REPLACEMENT="ext.kotlin_version = '1.3.72'"
 
 
 GRALDE_PATTERN="com.android.tools.build:gradle"
-GRALDE_PATTERN_REPLACEMENT="        classpath 'com.android.tools.build:gradle:3.6.3'\n"
+GRALDE_PATTERN_REPLACEMENT="        classpath 'com.android.tools.build:gradle:3.6.3'"
 
 
 GRADLE_WRAPPER_PATTERN="distributionUrl=https\://services.gradle.org/distributions/"
-GRADLE_WRAPPER_PATTERN_REPLACEMENT="distributionUrl=https\://services.gradle.org/distributions/gradle-5.6.4-all.zip\n"
+GRADLE_WRAPPER_PATTERN_REPLACEMENT="distributionUrl=https\://services.gradle.org/distributions/gradle-6.2-bin.zip"
 
 
 COMPILESDK_PATTERN="compileSdkVersion"
-COMPILESDK_PATTERN_REPLACEMENT="    compileSdkVersion 28\n"
+COMPILESDK_PATTERN_REPLACEMENT="    compileSdkVersion 28"
 
 BUILD_TOOLS_PATTERN="buildToolsVersion"
-BUILD_TOOLS_PATTERN_REPLACEMENT="    buildToolsVersion '28.0.3'\n"
+BUILD_TOOLS_PATTERN_REPLACEMENT="    buildToolsVersion '28.0.3'"
 
 COMPILE_SUPPORT_V7_PATTERN="com.android.support:appcompat-v7"
-COMPILE_SUPPORT_V7_PATTERN_REPLACEMENT="    implementation 'com.android.support:appcompat-v7:28.0.0'\n"
+COMPILE_SUPPORT_V7_PATTERN_REPLACEMENT="    implementation 'com.android.support:appcompat-v7:28.0.0'"
 
 COMPILE_SUPPORT_RECYCLER_VIEW_PATTERN="com.android.support:recyclerview-v7"
-COMPILE_SUPPORT_RECYCLER_VIEW_PATTERN_REPLACEMENT="    implementation 'com.android.support:recyclerview-v7:28.0.0'\n"
+COMPILE_SUPPORT_RECYCLER_VIEW_PATTERN_REPLACEMENT="    implementation 'com.android.support:recyclerview-v7:28.0.0'"
 
 CONSTRAINT_LAYOUT="com.android.support.constraint:constraint-layout"
-CONSTRAINT_LAYOUT_REPLACEMENT="    implementation 'com.android.support.constraint:constraint-layout:1.1.3'\n"
+CONSTRAINT_LAYOUT_REPLACEMENT="    implementation 'com.android.support.constraint:constraint-layout:1.1.3'"
 
 GLIDE_PATTERN="com.github.bumptech.glide:glide"
-GLIDE_PATTERN_REPLACEMENT="    implementation 'com.github.bumptech.glide:glide:4.7.1'\n"
+GLIDE_PATTERN_REPLACEMENT="    implementation 'com.github.bumptech.glide:glide:4.7.1'"
 
 OKHTTP3_PATTERN="com.squareup.okhttp3:okhttp:"
-OKHTTP3_PATTERN_REPLACEMENT="    implementation 'com.squareup.okhttp3:okhttp:3.10.0'\n"
+OKHTTP3_PATTERN_REPLACEMENT="    implementation 'com.squareup.okhttp3:okhttp:3.11.0'"
 
 RXJAVA_PATTERN="io.reactivex.rxjava2:rxjava"
-RXJAVA_PATTERN_REPLACEMENT="    implementation 'io.reactivex.rxjava2:rxjava:2.2.4'\n"
+RXJAVA_PATTERN_REPLACEMENT="    implementation 'io.reactivex.rxjava2:rxjava:2.2.6'"
 
 RX_ANDROID_PATTERN="io.reactivex.rxjava2:rxandroid"
-RX_ANDROID_PATTERN_REPLACEMENT="    implementation 'io.reactivex.rxjava2:rxandroid:2.1.1'\n"
+RX_ANDROID_PATTERN_REPLACEMENT="    implementation 'io.reactivex.rxjava2:rxandroid:2.1.1'"
 
 RETROFIT_PATTERN="com.squareup.retrofit2:retrofit"
-RETROFT_PATTERN_REPLACEMENT="    implementation 'com.squareup.retrofit2:retrofit:2.5.0'\n"
+RETROFT_PATTERN_REPLACEMENT="    implementation 'com.squareup.retrofit2:retrofit:2.5.0'"
 
 
 TEST_RUNNNER_PATTERN="com.android.support.test:runner"
-TEST_RUNNNER_PATTERN_REPLACEMENT="androidTestImplementation 'com.android.support.test:runner:1.0.2'\n"
+TEST_RUNNNER_PATTERN_REPLACEMENT="    androidTestImplementation 'com.android.support.test:runner:1.0.2'"
 
 TEST_ESPRESSO_PATTERN="com.android.support.test.espresso:espresso-core"
-TEST_ESPRESSO_PATTERN_REPLACEMENT="    androidTestImplementation 'com.android.support.test.espresso:espresso-core:3.0.2'\n"
+TEST_ESPRESSO_PATTERN_REPLACEMENT="    androidTestImplementation 'com.android.support.test.espresso:espresso-core:3.0.2'"
+
+ANDROID_SUPPORT_DESIGN_PATTERN="com.android.support:design:"
+ANDROID_SUPPORT_DESIGN_PATTERN_REPLACEMENT="    implementation 'com.android.support:design:28.0.0'"
+
 
 JUNIT_PATTER="junit:junit:"
-JUNIT_PATTER_REPLACE_MENT="    testImplementation 'junit:junit:4.13'\n"
+JUNIT_PATTER_REPLACE_MENT="    testImplementation 'junit:junit:4.13'"
 
 ANDROIDX_APPCOMPAT="androidx.appcompat:appcompat"
-ANDROIDX_APPCOMPAT_REPLACEMENT="    implementation 'androidx.appcompat:appcompat:1.1.0'\n"
+ANDROIDX_APPCOMPAT_REPLACEMENT="    implementation 'androidx.appcompat:appcompat:1.1.0'"
 
 
 REPLACEMENT_DICT_1 = {EXT_KOTLIN_PATTERN:EXT_KOTLIN_PATTERN_REPLACEMENT
@@ -95,7 +99,8 @@ REPLACEMENT_DICT_3 = {COMPILESDK_PATTERN:COMPILESDK_PATTERN_REPLACEMENT,
     RX_ANDROID_PATTERN:RX_ANDROID_PATTERN_REPLACEMENT,
     RETROFIT_PATTERN:RETROFT_PATTERN_REPLACEMENT,
     COMPILE_SUPPORT_RECYCLER_VIEW_PATTERN:COMPILE_SUPPORT_RECYCLER_VIEW_PATTERN_REPLACEMENT,
-    GRALDE_PATTERN:GRALDE_PATTERN_REPLACEMENT
+    GRALDE_PATTERN:GRALDE_PATTERN_REPLACEMENT,
+    ANDROID_SUPPORT_DESIGN_PATTERN:ANDROID_SUPPORT_DESIGN_PATTERN_REPLACEMENT
     }
 
 
@@ -110,23 +115,24 @@ def replace(file_path, userdict):
                 for key in keys:
                     if key in line :
                         new_file.write(userdict[key])
+                        new_file.write('\n')
                         # print ('found key  {0}, replace with {1}'.format(key , userdict[key]))
                         found = True
                         break
                     else:
                         pass
-                        # print("failed to found {0} in {1} ".format(key, line))    
+                        # print("failed to found {0} in {1} ".format(key, line))
                 if not found:
                     new_file.write(line)
-             
+
     #Copy the file permissions from the old file to the new file
     copymode(file_path, abs_path)
     #Remove original file
     remove(file_path)
     #Move new file
-    move(abs_path, file_path)    
+    move(abs_path, file_path)
 
-def filter_large_gradle(): 
+def filter_large_gradle():
     print("start==")
     replace(LARGE_GRADLE_FILE,REPLACEMENT_DICT_1)
     print("end==")
@@ -154,7 +160,10 @@ def maybe_multiple_module():
             if(os.path.exists(gradlefile)):
                 # print('file  {0}  exists '.format(gradlefile))
                 replace(gradlefile,REPLACEMENT_DICT_3)
-
+            gradle_wrapper_file = os.path.join(abspath,"gradle/wrapper/gradle-wrapper.properties")
+            if(os.path.exists(gradle_wrapper_file)):
+                # print('file  {0}  exists '.format(gradlefile))
+                replace(gradle_wrapper_file,REPLACEMENT_DICT_2)
 
 
 def main():
@@ -182,4 +191,4 @@ if __name__ == "__main__":
     main()
 
 
-    
+
