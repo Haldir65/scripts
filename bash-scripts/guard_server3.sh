447216d4c11c9547e327a7d0d90a3b879b0edcb1
mjw090608@gmail.com <mjw090608@gmail.com>
2021-01-08 19:40:21 +0800

    updates

---
+++ bash-scripts/guard_server3.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+#set -x
+
+IPSET_WHILTE_LIST="whitelist"
+IPSET="/sbin/ipset"
+TMPFILE=$(mktemp)
+
+HOME_IP_RANGE = ['a','b','c','d']
+
+
+. $(dirname "$0")/functions.sh
+
+
+
+
+function processExistingTables() {
+    local IPSET_OUTPUT="${IPSET} list ${IPSET_WHILTE_LIST}"
+    local myarr = ($(echo IPSET_OUTPUT | awk '{if (NR>8) print $1 }' | awk -F '.' '{ printf "%s%s\n", $1, $2}' | sort | uniq))
+    local array=( 'one' 'two' 'three' )
+    
+    for i in "${HOME_IP_RANGE[@]}"
+    do
+        echo $i
+    done
+}
+
+
+function addMyHomeIpRangeToIpset(){
+
+}
+
+function main(){
+    _green "this is imported funtions working \n"
+    processExistingTables
+
+
+    _red "yes imported is working\n"
+}
+
+
+main
