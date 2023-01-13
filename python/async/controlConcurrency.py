cf6ecbc37df29f1426bd300081162a60d9c3d194
mjw090608@gmail.com <mjw090608@gmail.com>
2023-01-13 18:26:44 +0800

    update some aiofiles

--- python/async/controlConcurrency.py
+++ python/async/controlConcurrency.py
@@ -1,8 +1,9 @@
 import asyncio
 import aiohttp
+import aiofiles
 
 CONCURRENCY = 5
-URL = 'https://www.baidu.com'
+URL = 'https://www.zhihu.com'
 
 semaphore = asyncio.Semaphore(CONCURRENCY)
 session = None
@@ -13,14 +14,17 @@ async def scrape_api():
         print('scraping', URL)
         async with session.get(URL) as response:
             await asyncio.sleep(1)
-            return await response.text()
+            content =  await response.text()
+            async with aiofiles.open('content.html', mode='w') as f:
+                await f.write(content)
 
 async def main():
     global session
     session = aiohttp.ClientSession()
-    scrape_index_tasks = [asyncio.ensure_future(scrape_api()) for _ in range(10000)]
+    scrape_index_tasks = [asyncio.ensure_future(scrape_api()) for _ in range(6)]
     await asyncio.gather(*scrape_index_tasks)
+    await session.close()
 
 ## https://cuiqingcai.com/202272.html
 if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(main())
+    asyncio.get_event_loop().run_until_complete(main()) 
