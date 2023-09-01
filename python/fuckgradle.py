a31a7b522be1133c0b45d59aa12ff940b467ab0f
Haldir65 <mjw090608@gmail.com>
2023-09-01 15:07:36 +0800

    bump version

--- python/fuckgradle.py
+++ python/fuckgradle.py
@@ -47,11 +47,11 @@ APP_BUILD_GRADLE_FILE = "app/build.gradle"
 ## ext.kotlin_version = '1.3.61'
 
 
-KOTLIN_VERSION="1.9.0"
+KOTLIN_VERSION="1.9.10"
 EXT_KOTLIN_PATTERN="ext.kotlin_version"
 EXT_KOTLIN_PATTERN_REPLACEMENT="    ext.kotlin_version = '{0}'".format(KOTLIN_VERSION)
 
-AGP_VERSION="8.1.0"
+AGP_VERSION="8.1.1"
 
 GRALDE_PATTERN="classpath 'com.android.tools.build:gradle:" ## implementation 'com.android.tools.build:gradle:4.1.1' in gradle plugin shouldn't match
 GRALDE_PATTERN_REPLACEMENT="classpath 'com.android.tools.build:gradle:{0}'".format(AGP_VERSION)
